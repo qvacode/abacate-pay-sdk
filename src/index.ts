8f137908f347e5d1b1345d566db3eaf73b6360a1
Ernesto <david.hernandez@gosocket.com>
2025-09-27 15:09:19 -0300

    feat: agregar proveedores de tienda y retiro al SDK

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { BillingProvider } from './modules/billing/billing.provider';
 import { CustomerProvider } from './modules/customer/customer.provider';
 import { PixProvider } from './modules/pix/pix.provider';
+import { StoreProvider } from './modules/store/store.provider';
+import { WithdrawProvider } from './modules/withdraw/wd.provider';
 
 export class AbacatepaySDK {
     public customer: CustomerProvider;
     public billing: BillingProvider;
-    public pix: PixProvider
+    public pix: PixProvider;
+    public withdraw: WithdrawProvider;
+    public store: StoreProvider;
 
     constructor(token: string) {
         this.customer = new CustomerProvider(token);
         this.billing = new BillingProvider(token);
-        this.pix = new PixProvider(token)
+        this.pix = new PixProvider(token);
+        this.withdraw = new WithdrawProvider(token);
+        this.store = new StoreProvider(token);
     }
 }
