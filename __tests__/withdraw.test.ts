2a955a47e0f2f477b80cdef82c144f2a90464aff
Ernesto <david.hernandez@gosocket.com>
2025-09-27 15:09:13 -0300

    test: agregar pruebas para métodos de retiro

---
+++ __tests__/withdraw.test.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { AbacatepaySDK } from '../src';
+import { withdrawMock } from '../src/mock/withdraw';
+
+describe('pix payment methods', () => {
+    const token = process.env.TOKEN || 'tu_token';
+    const SDK = new AbacatepaySDK(token);
+
+    test('should create new withdraw', async () => {
+        const response = await SDK.withdraw.create(withdrawMock);
+        console.dir(response);
+    });
+
+    test('should get withdraw by external id', async () => {
+        const id = '1234567890'
+        const response = await SDK.withdraw.findOne(id);
+        console.dir(response);
+    });
+
+    test('should get all withdraws', async () => {
+        const response = await SDK.withdraw.list();
+        console.dir(response);
+    });
+});
